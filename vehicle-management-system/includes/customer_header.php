f259999639dccbecb526fba8165376e5a564d0cd
kindredjebguinto <kindredjeb.guinto@neu.edu.ph>
2026-08-27 13:21:18 +0800

    Update customer_header.php

--- vehicle-management-system/includes/customer_header.php
+++ vehicle-management-system/includes/customer_header.php
@@ -1,1 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
 
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Vehicle Service Management System</title>
+
+<link rel="stylesheet" href="css/style.css">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+
+<div class="container">
+
+<a class="navbar-brand fw-bold" href="index.php">🚗 VSMS</a>
+
+<button class="navbar-toggler"
+type="button"
+data-bs-toggle="collapse"
+data-bs-target="#menu">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="menu">
+
+  <ul class="navbar-nav ms-auto">
+
+    <li class="nav-item">
+      <a href="index.php" class="nav-link">Home</a>
+    </li>
+
+    <li class="nav-item">
+      <a href="about.php" class="nav-link">About</a>
+    </li>
+
+    <li class="nav-item">
+      <a href="services.php" class="nav-link">Services</a>
+    </li>
+
+    <li class="nav-item">
+      <a href="booking.php" class="nav-link">Book Service</a>
+    </li>
+
+    <li class="nav-item">
+      <a href="contact.php" class="nav-link">Contact</a>
+    </li>
+
+    <li class="nav-item">
+      <a href="login.php" class="btn btn-primary ms-2">Login</a>
+    </li>
+
+  </ul>
+
+</div>
+
+</div>
+
+</nav> 
